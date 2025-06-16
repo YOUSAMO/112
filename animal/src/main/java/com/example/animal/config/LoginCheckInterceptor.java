013bf01d78386fc1c4bee45c48f2250056edaf3d
jake8771 <jake525@naver.com>
2025-06-16 22:35:43 +0900

    관리자페이지 수정 및 삭제 추가함

--- animal/src/main/java/com/example/animal/config/LoginCheckInterceptor.java
+++ animal/src/main/java/com/example/animal/config/LoginCheckInterceptor.java
@@ -1,5 +1,9 @@
 package com.example.animal.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.util.AntPathMatcher;
 import org.springframework.web.servlet.HandlerInterceptor;
 //private: 이 필드는 클래스 내부에서만 접근할 수 있다는 뜻이야. 외부에서는 접근 불가.
 // static: 이 필드는 인스턴스 생성 없이 클래스 차원에서 공유된다는 뜻이야.
@@ -9,7 +13,6 @@ import org.springframework.web.servlet.HandlerInterceptor;
 public class LoginCheckInterceptor implements HandlerInterceptor {
 
 
-    /*
     // AntPathMatcher: Spring에서 제공하는 경로 매칭 도구 클래스야. 패턴 문자열("/mypage/**", "/css/*" 등)과 실제URI를 비교할 때 사용해
     // new AntPathMatcher(): 이 클래스의 인스턴스를 하나 생성하는 거야.
 
@@ -32,7 +35,7 @@ public class LoginCheckInterceptor implements HandlerInterceptor {
         }
         return true;
     }
-    */
+
 
 
 }
